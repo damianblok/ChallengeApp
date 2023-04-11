3bb61e7c1e81f1350b3e007f9fd0e24196b7e6b5
damianblok <dabliu2018@gmail.com>
2023-04-12 00:54:14 +0200

    Stworzenie aplikacji

---
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -0,0 +1,24 @@
+﻿string TwojeImię = "Damian";
+string TwojaPłeć = "Mężczyzna";
+var TwójWiek = 37;
+string Tekst1 = " - jesteś mężczyzną w wieku 37 lat.";
+string Tekst2 = " - jesteś mężczyzną poniżej 30 lat.";
+string result1 = TwojeImię + Tekst1;
+string result2 = TwojeImię + Tekst2;
+
+if (TwojaPłeć == "mężczyzna" && TwójWiek == 37)
+{
+	Console.WriteLine(result1);
+}
+else if (TwojaPłeć == "mężczyzna" && TwójWiek < 30)
+{
+	Console.WriteLine(result2);
+}
+else if (!(TwojaPłeć == "mężczyzna") && TwójWiek < 18)
+{
+	Console.WriteLine("Jesteś niepełnoletnią osobą.");
+}
+else if (TwójWiek > 30)
+{
+	Console.WriteLine("Jesteś już po trzydziestce");
+}
